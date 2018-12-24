fae38a9d82e09866dd90428104a4322a95203ec7
Brend Wanders <brend@13w.nl>
2018-12-24 13:08:10 +0100

    Add item filtering to reader view

--- glassball/static/script.js
+++ glassball/static/script.js
@@ -1,13 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.querySelectorAll('.selector.item').forEach(function(el) {
+    function items() {
+        return document.querySelectorAll('.selector.item');
+    }
+
+    function filters() {
+        return document.querySelectorAll('.selector.filter');
+    }
+
+    function uiSelect(collection, entry) {
+        collection().forEach(function(el) {
+            el.classList.remove('selector--selected');
+        })
+        entry.classList.add('selector--selected');
+    }
+
+    items().forEach(function(el) {
         el.addEventListener('click', function() {
-            document.querySelectorAll('.selector.item').forEach(function(el) {
-                el.classList.remove('selector--selected');
-            })
-            el.classList.add('selector--selected');
+            // Update UI for newly selected item
+            uiSelect(items, el);
+
+            // Navigate item viewer to item page
             var itemUrl = el.dataset.item + '.html';
             document.querySelector('#item-view').src = itemUrl;
+        });
+    });
+
+
+    var filterTypes = {
+        all: function() {
+            items().forEach(function(e) {
+                e.classList.remove('hidden');
+            });
+        },
+        feed: function(options) {
+            var feed = options.feed;
+            items().forEach(function(e) {
+                if(e.dataset.feed == feed) {
+                    e.classList.remove('hidden');
+                } else {
+                    e.classList.add('hidden');
+                }
+            });
+        }
+    }
+
+    filters().forEach(function(el) {
+        el.addEventListener('click', function() {
+            // Update UI for newly selected filter
+            uiSelect(filters, el);
+
+            // Filter item list
+            filterTypes[el.dataset.filterType](el.dataset);
         });
     });
 
